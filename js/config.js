cd207683c072b858d529d2b582b202b6ff4b59c0
Sahir Memon <sahir.memon+serverless@gmail.com>
2019-05-02 16:30:37 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_UCBbgX5Ga', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1h7ig02chfhco36l8egp7fkb9r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
